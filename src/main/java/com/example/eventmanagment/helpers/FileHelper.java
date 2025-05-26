4bf41450a5f5beec8c592f5abdbfc01f6fa1e509
Daor SvircaE <daor.svirca1@gmail.com>
2025-05-26 15:27:39 +0200

    Changed the FileHelper to support the image url.

---
+++ src/main/java/com/example/eventmanagment/helpers/FileHelper.java
@@ -0,0 +1,5 @@
+package com.example.eventmanagment.helpers;
+
+public interface FileHelper {
+    String uploadFile(String folder, String fileName, byte[] bytes);
+}
---
+++ src/main/java/com/example/eventmanagment/helpers/impls/FileHelperImpl.java
@@ -0,0 +1,47 @@
+package com.example.eventmanagment.helpers.impls;
+
+
+import com.example.eventmanagment.helpers.FileHelper;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+
+import java.nio.file.Paths;
+
+import java.util.UUID;
+
+@Component
+public class FileHelperImpl implements FileHelper {
+
+    @Override
+    public String uploadFile(String folder, String fileName, byte[] bytes) {
+
+        try {
+
+            if (!Files.exists(Paths.get(folder))) {
+
+                Files.createDirectories(Paths.get(folder));
+
+            }
+
+            String newFileName = UUID.randomUUID() + "-" + fileName;
+
+            Path filePath = Paths.get(Paths.get(folder).toString(), newFileName);
+
+            Files.write(filePath, bytes);
+
+            return newFileName;
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+            return null;
+
+        }
+
+    }
+
+}
